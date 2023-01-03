9142bf323f1d79b512616627951c603c719b7fe8
LeeJay0126 <topaz9889@gmail.com>
2023-01-02 19:28:15 -0800

    .

---
+++ leebrothersfe/src/Pages/MainPage/MainPage.js
@@ -0,0 +1,11 @@
+import Nav from "../../Components/Nav/Nav";
+
+const MainPage = () => {
+    return (
+        <div className="App">
+            <Nav/>
+        </div>
+    );
+};
+
+export default MainPage;
